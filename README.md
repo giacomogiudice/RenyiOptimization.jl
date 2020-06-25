9b4c8a4a4e7d18002bb362e08710a73d98194062
Giacomo Giudice <giacomo.giudice@mail.com>
2020-06-25 18:23:40 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# RenyiOptimization.jl
+
+[![CI][ci-img]][ci-url]
+[![codecov][codecov-img]][codecov-url]
+
+[ci-img]: https://github.com/giacomogiudice/RenyiOptimization.jl/workflows/CI/badge.svg
+[ci-url]: https://github.com/giacomogiudice/RenyiOptimization.jl/actions?query=workflow%3ACI
+
+[codecov-img]: https://codecov.io/gh/giacomogiudice/RenyiOptimization.jl/branch/master/graph/badge.svg?token=fQlukhogec
+[codecov-url]: https://codecov.io/gh/giacomogiudice/RenyiOptimization.jl/
+
+## Installation
+
+Currently this package is not registered.
+Enter `]` in the REPL to access the package manager and type
+```
+pkg> add https://github.com/giacomogiudice/RenyiOptimization.jl/
+```
+to add the package to the current environment.
+
+## Compatibility
+
+The package is compatible with Julia 1.4 and above.
+
--- test/fixedpoints.jl
+++ test/fixedpoints.jl
@@ -1,5 +1,6 @@
 @testset "Testset for type $(T)" for T in (Float32, Float64, ComplexF32, ComplexF64)
     tol = 10*eps(real(T))
+    atol = 2*tol
 
     phys_spaces = (ℂ^2, ℂ^4, ℂ^3)
     virt_spaces = (ℂ^2, ℂ^10, ℂ^4)
@@ -23,30 +24,30 @@
         @testset "Single fixed points" begin
             ρL, ρR, ζ, ∇ζ = @inferred singlefixedpoints(AL; tol = tol)
 
-            @test ζ ≈ 1 atol = tol
-            @test ρL ≈ one(ρL) atol = tol
-            @test ρL ⊙ ρR ≈ 1 atol = tol
-            @test righttransfer(ρR, AL) ≈ ρR atol = tol
-            @test (∇ζ ⋅ AL)/2 ≈ 1 atol = tol
+            @test ζ ≈ 1 atol = atol
+            @test ρL ≈ one(ρL) atol = atol
+            @test ρL ⊙ ρR ≈ 1 atol = atol
+            @test righttransfer(ρR, AL) ≈ ρR atol = atol
+            @test (∇ζ ⋅ AL)/2 ≈ 1 atol = atol
         end
 
         @testset "Double fixed points" begin
             ΣL, ΣR, η, ∇η = @inferred doublefixedpoints(AL; tol = tol)
 
-            @test ΣL ⊙ ΣR ≈ 1 atol = tol
-            @test norm(leftdoubletransfer(ΣL, AL) - η*ΣL, Inf) ≈ 0 atol = tol
-            @test norm(rightdoubletransfer(ΣR, AL) - η*ΣR, Inf) ≈ 0 atol = tol
-            @test (∇η ⋅ AL)/2 ≈ η atol = tol
+            @test ΣL ⊙ ΣR ≈ 1 atol = atol
+            @test norm(leftdoubletransfer(ΣL, AL) - η*ΣL, Inf) ≈ 0 atol = atol
+            @test norm(rightdoubletransfer(ΣR, AL) - η*ΣR, Inf) ≈ 0 atol = atol
+            @test (∇η ⋅ AL)/2 ≈ η atol = atol
         end
 
         @testset "Energy fixed points" begin
             ρL, ρR, _, _ = singlefixedpoints(AL; tol = tol)
             HL, HR, ε, ∇ε = @inferred energyfixedpoints(AL, H, ρL, ρR; tol = tol)
 
-            @test HL ⊙ ρR ≈ 0 atol = tol
-            @test ρL ⊙ HR ≈ 0 atol = tol
-            @test ε ≈ expectationvalue(O, AL) atol = tol
-            @test (∇ε ⋅ AL)/2 ≈ ε atol = tol
+            @test HL ⊙ ρR ≈ 0 atol = atol
+            @test ρL ⊙ HR ≈ 0 atol = atol
+            @test ε ≈ expectationvalue(O, AL) atol = atol
+            @test (∇ε ⋅ AL)/2 ≈ ε atol = atol
         end
     end 
 end
